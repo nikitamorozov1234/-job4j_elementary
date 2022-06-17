ba2357747a86680e2379a09fe99677b628d24fcd
Nikita Morozov <nikitamorozov748@gmail.com>
2022-06-17 14:09:57 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        String idea = "I Like Java";
+        System.out.println(idea);
+        idea = idea +  " But i am a Newbie ";
+        int year = 2022;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
